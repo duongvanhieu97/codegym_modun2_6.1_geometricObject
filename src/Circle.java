cd53a8447f18c3fe85739e75d3d05589a365880c
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-10 13:33:09 +0700

    update src

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,37 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return radius * Math.PI * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle width radius = " + getRadius() + ", which is a subclass of " + super.toString();
+    }
 }
